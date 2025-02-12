1cdb4108fb3294d8e4a9fa6df8f5a3f49f635507
Dev-NayanBiswas <dev.nayan.biswas@gmail.com>
2025-02-12 16:01:49 +0600

    review card updated

--- src/Components/Review/ReviewCard.jsx
+++ src/Components/Review/ReviewCard.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Rating from './Rating';
 
 const ReviewCard = ({reviewData}) => {
-  const {_id,serviceTitle,serviceID,rating,postedDate,name,image,email,comment}= reviewData || {}
+  const {serviceTitle,rating,postedDate,name,image,email,comment}= reviewData || {}
   return (
     <StyledWrapper>
       <div className="card !bg-[#20161f]/75">
@@ -19,13 +19,6 @@ const ReviewCard = ({reviewData}) => {
   );
 }
 
-
-
-{/* <svg className="h-8 w-8 shrink-0 fill-amber-400" viewBox="0 0 256 256">
-        <path
-            d="M239.2 97.4A16.4 16.4.0 00224.6 86l-59.4-4.1-22-55.5A16.4 16.4.0 00128 16h0a16.4 16.4.0 00-15.2 10.4L90.4 82.2 31.4 86A16.5 16.5.0 0016.8 97.4 16.8 16.8.0 0022 115.5l45.4 38.4L53.9 207a18.5 18.5.0 007 19.6 18 18 0 0020.1.6l46.9-29.7h.2l50.5 31.9a16.1 16.1.0 008.7 2.6 16.5 16.5.0 0015.8-20.8l-14.3-58.1L234 115.5A16.8 16.8.0 00239.2 97.4z">
-        </path>
-    </svg> */}
 
 const StyledWrapper = styled.div`
   .card {
